144cee1f1ff4f84ce85e231e81becfa9909fe47c
Aurelie Ferron <aurelie.ferron@haufe-lexware.net>
2024-04-26 10:57:20 +0200

    undo config

--- configurations.py
+++
@@ -1,6 +0,0 @@
-infura_api_key = 'd41d2ad5d85e4e6383d7750030b1af97'
-eth_scan_api_key = 'A3UNV5BPTMEXUUSYES5VY12KE2FBQ42HQ6'
-tenderly_access_token = 'OLppgTbhR912L-qeLULZU8mO00N4P97Q'
-csv_file_path = 'data/input/incident1.csv'
-name_of_incident = 'incident1'
-infuria_url = f"https://mainnet.infura.io/v3/{infura_api_key}"
